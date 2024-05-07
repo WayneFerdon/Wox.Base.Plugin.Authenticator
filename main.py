378adae0794d1e1abbad02c0f19127c2e84a3cdc
WayneFerdon <wayneferdon@hotmail.com>
2024-05-07 17:09:04 +0800

    update ranamed dependences

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from WoxPluginBase_Query import *
+from WoxBasePluginQuery import *
 from AuthenticatorClient import *
 
 class Authenticator(QueryPlugin):
